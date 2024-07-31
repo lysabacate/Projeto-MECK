d5eb830e7ac66e066d58ca2da6a99fa62d1097b3
lysabacate <133353329+lysabacate@users.noreply.github.com>
2024-07-31 22:36:06 +0000

    linkando css e testando execução

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def paginainicial():
+    return render_template('pagina-inicial.html')
+
 if __name__ == '__main__':
     app.run()
