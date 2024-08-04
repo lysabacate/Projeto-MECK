1e2ed8948935dc25519ec550f97bf61e26e0f380
MuriloCs12 <133374714+MuriloCs12@users.noreply.github.com>
2024-08-04 16:39:39 +0000

    enviando progresso

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/aaa')
 def paginainicial():
     return render_template('pagina-inicial.html')
+
+@app.route('/')
+def entrar():
+    return render_template('pagina-login.html')
+
 
 if __name__ == '__main__':
     app.run()
