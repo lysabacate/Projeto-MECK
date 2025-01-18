3004843bed35b6e3f44866cf5984d5ee719ab369
lysabacate <133353329+lysabacate@users.noreply.github.com>
2025-01-18 15:46:20 +0000

    adicionando alguns códigos iniciais da integração com o bd

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template, request, flash, redirect
 import json
+from utils import db
+import os
+from flask_migrate import Migrate
+from flask_migrate import Migrate 
+from models.Usuario import Usuario
+
+
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'abuble'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+db_usuario = os.getenv('DB_USERNAME')
+db_senha = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_mydb = os.getenv('DB_DATABASE')
+
+conexao = f"mysql+pymysql://{db_usuario}:{db_password}@{db_host}/{db_mydb}"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def paginainicial():
