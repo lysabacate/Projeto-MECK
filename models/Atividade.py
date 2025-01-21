7addf1e7dddd3184f955de4c685be8044dc72634
lysabacate <133353329+lysabacate@users.noreply.github.com>
2025-01-21 10:40:50 +0000

    corrigindo errinhos

--- models/Atividade.py
+++ models/Atividade.py
@@ -2,19 +2,19 @@ from utils import db
 
 #Tabela usada no exemplo da prof:
 class Atividade(db.Model):
-	__tablename__= "atividade"
-	id = db.Column(db.Integer, primary_key = True)
-	codigo_prof = db.Column(db.Integer, db.ForeignKey('professor.codigo')
-    numeracao_nv = db.Column(db.Integer, db.ForeignKey('nivel.numeracao')
+    __tablename__= "atividade"
+    id = db.Column(db.Integer, primary_key = True)
+    codigo_prof = db.Column(db.Integer, db.ForeignKey('professor.codigo'))
+    numeracao_nv = db.Column(db.Integer, db.ForeignKey('nivel.numeracao'))
     nome = db.Column(db.String(100))
     questoes = db.Column(db.String(100))
     habilidade_eng = db.Column(db.String(100))
 	#respostas = db.Column(coloca oq aqui plmdds??)
 
-    professor = db.relationship('professor', foreign_keys=codigo_prof)
-    nivel = db.relationship('nivel', foreign_keys=numeracao_nv)
+    professor = db.relationship('professor', foreign_keys=[codigo_prof])
+    nivel = db.relationship('nivel', foreign_keys=[numeracao_nv])
 
-    def __init__(self, nome, email, senha):
+    def __init__(self, codigo_prof, numeracao_nv, questoes, habilidade_eng):
         self.codigo_prof = codigo_prof
         self.numeracao_nv = numeracao_nv
         self.questoes = questoes
