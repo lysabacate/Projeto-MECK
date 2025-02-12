a85bb7987e1dfbbf800a45a4bed24e2dbd3d1300
lysabacate <133353329+lysabacate@users.noreply.github.com>
2025-02-12 00:11:42 +0000

    adicionando olho mágicona pg registrar, excluindo campo email e add flash message

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,9 @@
 const password = document.getElementById('password');
 const icon = document.getElementById('icon');
 
+const confirm_password = document.getElementById('confirm_password');
+const icon_confirm_password = document.getElementById('icon_confirm_password');
+
 function showHide(){
     if(password.type == 'password'){
         password.setAttribute('type','text')
@@ -11,3 +14,15 @@ function showHide(){
         icon.classList.remove('hide')
     }
 }
+
+function showHide2(){
+    if(confirm_password.type == 'password'){
+        confirm_password.setAttribute('type','text')
+        icon_confirm_password.classList.add('hide')
+    }
+    else{
+        confirm_password.setAttribute('type','password')
+        icon_confirm_password.classList.remove('hide')
+    }
+}
+
