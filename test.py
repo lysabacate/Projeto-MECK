ac85c73ef0156e9718d0b1e20cadeac07d5bdc7b
MuriloCs12 <133374714+MuriloCs12@users.noreply.github.com>
2025-02-17 23:48:36 +0000

    Apagando test

--- test.py
+++
@@ -1,8 +0,0 @@
-my_string = "hello world"
-
-# Calculate the hash value of the string
-hash_value = hash(my_string)
-
-# Print the string and its hash value
-print("String: ", my_string)
-print("Hash value: ", hash_value)
